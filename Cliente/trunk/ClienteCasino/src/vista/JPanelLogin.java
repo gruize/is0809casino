c9a863f5722e6acab056495e7e02046fd0c93e29
ferlodavid <ferlodavid@cc0f0ba8-a479-11dd-a8c6-677c70d10fe4>
2009-04-16 11:45:14 +0000

    Añadidas la interfaz del login

---
+++ Cliente/trunk/ClienteCasino/src/vista/JPanelLogin.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vista;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import controlador.ControladorCliente;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+/**
+ *
+ * @author david
+ */
+public class JPanelLogin extends JPanel{
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private ControladorCliente controlador;
+
+     public JPanelLogin(ControladorCliente control){
+
+          controlador = control;
+          jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        jTextField1.setText("");
+
+        jTextField2.setText("");
+
+        jLabel1.setText("USUARIO:");
+
+        jLabel2.setText("PASSWORD:");
+
+        jButton1.setText("ACEPTAR");
+
+        jButton2.setText("CANCELAR");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))
+                        .addGap(188, 188, 188))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE))
+                        .addGap(37, 37, 37)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 122, Short.MAX_VALUE))
+                        .addGap(37, 37, 37))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35))
+        );
+     }
+     public JButton getBotonAceptar() {
+        return jButton1;
+    }
+     public String getUsuario(){
+         return jTextField1.getText();
+     }
+     public String getPassword(){
+         return jTextField2.getText();
+     }
+}
---
+++ Cliente/trunk/ClienteCasino/src/vista/VistaLogin.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vista;
+
+import java.util.Observer;
+import java.util.Observable;
+import controlador.ControladorCliente;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ *
+ * @author david
+ */
+public class VistaLogin extends JFrame implements Observer  {
+
+    private ControladorCliente controlador;
+    private JPanelLogin PanelLogin;
+    private OyenteEnviarMensajeLogin enviarMensajeLogin;
+
+    public VistaLogin(ControladorCliente control) {
+        super("Cliente Casino");
+        controlador = control;
+        inicializar();
+        ponerOyentes();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(332,174);
+		setVisible(true);
+        setResizable(false);
+        
+     }
+    private void inicializar() {
+
+        PanelLogin = new JPanelLogin(controlador);
+        add(PanelLogin);
+    }
+    private void ponerOyentes() {
+
+       enviarMensajeLogin = new OyenteEnviarMensajeLogin();
+
+       PanelLogin.getBotonAceptar().addActionListener(enviarMensajeLogin);
+    }
+    public void update(Observable o, Object arg) {
+        //if(obj instanceof
+        //Hacer lo que sea.
+    }
+    class OyenteEnviarMensajeLogin implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+         
+           if (controlador.conectar(PanelLogin.getUsuario(), PanelLogin.getPassword()));
+              System.out.println("conectado");
+        }
+
+    }
+}
