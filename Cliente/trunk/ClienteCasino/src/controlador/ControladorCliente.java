bf6a6b13f1a6fec378207765c5dde91aa1253739
asct86 <asct86@cc0f0ba8-a479-11dd-a8c6-677c70d10fe4>
2009-04-24 15:16:09 +0000

    Se generan los Mensajes de Jugada que recibirá el servidor. Falta aclarar con los de la Interfaz los parámetros que se enviarán para las apuestas de la Ruleta.

--- Cliente/trunk/ClienteCasino/src/controlador/ControladorCliente.java
+++ Cliente/trunk/ClienteCasino/src/controlador/ControladorCliente.java
@@ -8,8 +8,7 @@ package controlador;
 import comunicaciones.Comunicador;
 import java.io.IOException;
 import java.io.Serializable;
-import modelo.MensajeChat;
-import modelo.ModeloCliente;
+import modelo.*;
 /**
  *
  * @author david
@@ -53,6 +52,19 @@ public class ControladorCliente {
     public void enviarMensajeChat(String mensaje) {
         MensajeChat mensajeChat = new MensajeChat(modelo.getId(),1,mensaje,modelo.getUsuario());
         comunicador.enviarMensaje(1,mensajeChat);
+        //TODO poner TipoMensaje.MENSAJE_CHAT en lugar de 1
+    }
+    
+    /**
+     * Envía un mensaje de jugada (apuesta de ruleta, etc) hacia el servidor
+     * @param tipo: 
+         *  2- Mensaje de Jugada o Informacion de Salas y mesas
+         *  3- Mensaje de Entrada en mesa
+         *  4- Mensaje de Salida de Mesa
+     * @param mensaje
+     */
+    public void enviarMensajeJugada(int tipo, MensajeJugada mensaje){
+       comunicador.enviarMensaje(tipo, mensaje); 
     }
 
     public int getNumeroMesas(int sala) {
@@ -69,13 +81,74 @@ public class ControladorCliente {
         return 0;
     }
 
+    /**
+     * 
+     * @param totalApostado  lo que lleva apostado en la ronda ????  
+     * @param valor lo que se acaba de apostar en una casilla ????
+     */
     public void realizarApuesta(int totalApostado, String valor) {
         //modelo.realizarApuesta(totalApostado,valor);
         //Apuesta apuestaRealizada = new Apuesta(totalApostado,valor);
         //TODO:Enviar la apuesta al servidor
         //apuestaRealizada.imprimir();
+        
+        int idUsuario=modelo.getId();
+        int idMesa=modelo.getMesa();
+        String tipoApuesta="NUMERO"; //TODO cómo obtengo tipoApuesta?? valores: Numero,Color,ParImpar, Docena, Linea,FaltaPasa....
+        int casilla=1; //TODO casilla a la q se apuesta: al 21, al Rojo , a la 2º Docena,Falta, Par ...
+        int cantidadApostada=totalApostado; //TODO es totalApostado??? 
+        
+        //crear el objeto Jugada 
+        Jugada jugada=new Jugada(idUsuario, idMesa, tipoApuesta,casilla,cantidadApostada);
+        
+        //crear el objeto MensajeJugada 
+        MensajeJugada mensajeJugada=new MensajeJugada(idUsuario,idMesa,jugada);
+        
+        //enviar el mensaje hacia el servidor. 
+        enviarMensajeJugada(TipoMensaje.MENSAJE_JUGADA, mensajeJugada);
+        
     }
     
+    /**
+     * El usuario selecciona entrar en una mesa. Hay que enviar la solicitud al servidor
+     *
+     * @param idMesa identificador de la mesa
+     */
+    public void solicitudEntrarEnMesa(int idMesa){        
+        
+        //crear el objeto MensajeJugada (idUsuario, idMesa, Jugada)
+        MensajeJugada mensajeJugada=new MensajeJugada(modelo.getId(),idMesa,null);
+        
+        //enviar el mensaje hacia el servidor
+        enviarMensajeJugada(TipoMensaje.ENTRADA_MESA, mensajeJugada);
+    }
+    
+    /**
+     * El servidor ha incluido al jugador en la mesa. Hay que actualizar el modelo
+     * 
+     * @param mesa identificador de Mesa
+     */
+    public void confirmacionEntradaEnMesa(int mesa){
+        modelo.setMesa(mesa);
+    }
+    
+    /**
+     * El usuario selecciona salir de Mesa
+     */
+    public void salirDeMesa(){
+        
+        //crear el objeto MensajeJugada (idUsuario, idMesa, Jugada)
+        MensajeJugada mensajeJugada=new MensajeJugada(modelo.getId(),modelo.getMesa(),null);
+        
+        //enviar el mensaje hacia el servidor
+        enviarMensajeJugada(TipoMensaje.SALIDA_MESA, mensajeJugada);
+    }
+    
+    /**
+     * Cuando el servidor manda un mensaje al cliente. 
+     * @param tipo tipo del mensaje (1=chat, 2=MensajeJugada, 3=EntradaChat, 4=salidaMesa)
+     * @param mensaje
+     */
     public synchronized void mensajeRecibido(int tipo, Serializable mensaje){
         //TODO Este método se invoca cuando se recibe un mensaje
        if (tipo == 1){
@@ -84,6 +157,27 @@ public class ControladorCliente {
         }
         else if (tipo == 2) {
            //MensajeJugada
+            MensajeJugada mensajeJugada=(MensajeJugada)mensaje;
+            
+            /*
+             * Puedo obtener...
+             *              * 
+             * Para la ruleta:
+             *  - mensaje diciendo que ya no se puede apostar más porque se va a lanzar la bola
+             *  - mensaje de actualizacion de saldo, tras el lanzamiento de la bola
+             * 
+             * Para los dados: 
+             * 
+             */
+        }else if (tipo==TipoMensaje.ENTRADA_MESA){      
+            
+           //El servidor me confirma la entrada en la mesa
+            MensajeJugada mensajeJugada=(MensajeJugada)mensaje;            
+            //actualizar el modelo
+            confirmacionEntradaEnMesa(mensajeJugada.getMesa());
+            
+        }else{
+           System.err.println("No sé que tipo de mensaje me envía!! "+tipo);
         }
 
     }
---
+++ Cliente/trunk/ClienteCasino/src/modelo/TipoMensaje.java
@@ -0,0 +1,24 @@
+/*
+ * Tipos de mensajes que se envían al servidor
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Chaudhary
+ */
+public class TipoMensaje {
+
+    /**mensaje de chat*/
+    public static int MENSAJE_CHAT=1;
+    
+    /** mensaje de jugada(de ruleta, dados, ...)*/
+    public static int MENSAJE_JUGADA=2;
+    
+    /** mensaje de entrada en la mesa. Tanto de solicitud del cliente, como de confirmacion del servidor */
+    public static int ENTRADA_MESA=3;
+    
+    /** salida de la mesa. Lo solicita el cliente, pero no recibe confirmación del servidor*/
+    public static int SALIDA_MESA=4;
+}
