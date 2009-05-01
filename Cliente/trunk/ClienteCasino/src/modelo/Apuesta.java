8dbea64ea8716635054fe80748c46d203c191392
honguito42 <honguito42@cc0f0ba8-a479-11dd-a8c6-677c70d10fe4>
2009-05-01 16:56:05 +0000

    Clase apuesta mejorada. El atributo casilla indica a lo que se apuesta, es un String y sus valores pueden ser numeros desde 0 ... 36, Par, Impar, Rojo, Negro, Fila1,..., Docena1, ... Mitad1,...

--- Cliente/trunk/ClienteCasino/src/modelo/Apuesta.java
+++ Cliente/trunk/ClienteCasino/src/modelo/Apuesta.java
@@ -21,7 +21,7 @@ public class Apuesta {
 
     //Indica el valor al que se apuesta. Casilla 1, 2, ..., 36, 0, Par, Impar,
     //Rojo, Negro, Fila1, ... Docena1, ... Mitad1...
-    private casillaAp casilla;
+    private String casilla;
     //Tipo de casilla a la que se apuesta. Numero, docena, fila, color, par_impar, mitad
     private tipoAp tipo;
     private proporcionAp proporcion;
@@ -29,18 +29,29 @@ public class Apuesta {
     private int valorApostado;
 
     public Apuesta(casillaAp casilla, tipoAp tipo, proporcionAp proporcion, int valorApostado) {
-        this.casilla = casilla;
+        this.casilla = casilla.toString();
         this.tipo = tipo;
         this.proporcion = proporcion;
         this.valorApostado = valorApostado;
     }
 
-    public casillaAp getCasilla() {
+        public Apuesta(int casilla, tipoAp tipo, proporcionAp proporcion, int valorApostado) {
+        this.casilla = Integer.toString(casilla);
+        this.tipo = tipo;
+        this.proporcion = proporcion;
+        this.valorApostado = valorApostado;
+    }
+
+    public String getCasilla() {
         return casilla;
     }
 
     public void setCasilla(casillaAp casilla) {
-        this.casilla = casilla;
+        this.casilla = casilla.toString();
+    }
+
+    public void setCasilla(int casilla) {
+        this.casilla = Integer.toString(casilla);
     }
 
     public proporcionAp getProporcion() {
